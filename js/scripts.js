80a9007d8af4162554eb6789dafaca545e421dcc
Chelsea L Gifford <chelsealgifford@Chelseas-MacBook-Pro.local>
2019-12-11 16:26:08 -0800

    ideas

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var creams = ['strawberry', 'vanilla', 'chocolate'];
+var i = 0, length = list.length;
+for; (i, i < length; i++) {
+  console.log(list[i]); }
+  document.getElementById("creams").innerHTML = list;
+}
